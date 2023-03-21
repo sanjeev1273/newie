b0d9d38ff7064ef8e3c7480fbd7c60f9be40df00
sanjeev1273 <32138060+sanjeev1273@users.noreply.github.com>
2023-03-21 22:16:15 +0530

    intializing

---
+++ src/main/java/com/example/demo/Demospring.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Demospring implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
